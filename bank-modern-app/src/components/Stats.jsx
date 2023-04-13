16d230decfc092cdc45fc63d7005d08908c32804
Hamza Nachawati <Hamza.nachawati77@gmail.com>
2023-04-13 16:10:25 +0200

    done with stats component

--- bank-modern-app/src/components/Stats.jsx
+++ bank-modern-app/src/components/Stats.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "../style";
+import { stats } from "../constants";
 
-const Stats = () => {
-  return <div>Stats</div>;
-};
+const Stats = () => (
+  <section
+    className={`${styles.flexCenter} flex-row flex-wrap  sm:mb-mb-20 mb:6`}
+  >
+    {stats.map((state) => (
+      <div
+        key={state.id}
+        className={`flex-1 flex justify-start items-center flex-row m-3 `}
+      >
+        <h3 className=" font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading[53px] leading-[43px] text-white ">
+          {state.value}
+        </h3>
+        <p className=" font-poppins font-normal xs:text-[20px] text-[15px] xs:leading[26px] leading-[21px] text-gradient uppercase ml-3 ">
+          {state.title}
+        </p>
+      </div>
+    ))}
+  </section>
+);
 
 export default Stats;
